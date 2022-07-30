6b5b94119e7e5cb7557c7afa09f7492aedaa0bbb
fuaus <kotingfunk@gmail.com>
2022-07-30 11:07:52 -0400

    queries.sql: add select logic to show data content

--- db/queries.sql
+++ db/queries.sql
@@ -1,1 +1,15 @@
 -- running all read commands in SQL so you don't have to type it out in terminal
+USE employee_db;
+
+SELECT 
+employee.id,
+employee.first_name,
+employee.last_name,
+role.title,
+department.name,
+role.salary,
+CONCAT(mgr.first_name, " ", mgr.last_name) as manager 
+FROM employee
+LEFT JOIN role ON role.id=employee.role_id
+LEFT JOIN department ON role.department_id=department.id
+LEFT JOIN employee as mgr ON employee.id=mgr.manager_id;
--- db/schema.sql
+++ db/schema.sql
@@ -23,11 +23,11 @@ CREATE TABLE  employee(
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
-    manger_id INT,
+    manager_id INT,
     FOREIGN KEY(role_id)
     REFERENCES role(id)
     ON DELETE CASCADE,
-    FOREIGN KEY(manger_id)
+    FOREIGN KEY(manager_id)
     REFERENCES employee(id)
     ON DELETE SET NULL
 );
