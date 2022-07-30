638b875586e198492eacf56ffce77665caf73a44
fuaus <kotingfunk@gmail.com>
2022-07-30 10:57:42 -0400

    add values of insert in seeds.sql that will go into the blank tables of schema

--- db/schema.sql
+++ db/schema.sql
@@ -5,13 +5,13 @@ CREATE DATABASE employee_db;
 USE employee_db;
 CREATE TABLE department(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30)
+    name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(30),
-    salary DECIMAL(10,2),
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10,2) NOT NULL,
     department_id INT,
     FOREIGN KEY(department_id)
     REFERENCES department(id)
@@ -20,13 +20,13 @@ CREATE TABLE role(
 
 CREATE TABLE  employee(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
     role_id INT,
     manger_id INT,
     FOREIGN KEY(role_id)
     REFERENCES role(id)
-    ON DELETE CASCADE
+    ON DELETE CASCADE,
     FOREIGN KEY(manger_id)
     REFERENCES employee(id)
     ON DELETE SET NULL
