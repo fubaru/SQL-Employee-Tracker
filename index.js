8be4e3618fc25f055e3f9bb0c7a64f4a3817d20a
fuaus <kotingfunk@gmail.com>
2022-08-01 18:48:50 -0400

    index.js: create inrquirer prompt promise function for menu

--- index.js
+++ index.js
@@ -14,8 +14,30 @@ const menuQuestions = [
     }
 ]
 
+const employeeAddQuestions = [
+    {
+        type:"input",
+        name:"last_name",
+        message:"What is your first name?",
 
+    }
+]
 
 function menu() {
+    inquirer.prompt(menuQuestions)
+    .then(res=>{
+        if(res.menu==="view all employees"){
+            viewEmployees()
+        } else if (res==="add an employee"){
+            addEmployees()
+        }
+    })
+}
+
+function viewEmployees() {
+    
+}
+
+function addEmployees() {
 
 }
